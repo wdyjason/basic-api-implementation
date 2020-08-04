e3549e237940e7dd28cc0d1dda67da6d5d176740
wdyjason <ddd123321456555@qq.com>
2020-08-04 18:34:38 +0800

    finish rs/list api

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,21 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = Arrays.asList(new RsEvent("第一条事件", "经济"),
+          new RsEvent("第二条事件", "文化"), new RsEvent("第三条事件", "政治"));
+
+  @GetMapping("rs/list")
+  public List<RsEvent> getAllInList() {
+    return rsList;
+  }
+
 }
---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.JsonPath;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_get_rs_list_when_request_get_for_all() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName").value("第一条事件"))
+                .andExpect(jsonPath("$[0].keyWord").value("经济"))
+                .andExpect(jsonPath("$[1].eventName").value("第二条事件"))
+                .andExpect(jsonPath("$[1].keyWord").value("文化"))
+                .andExpect(jsonPath("$[2].eventName").value("第三条事件"))
+                .andExpect(jsonPath("$[2].keyWord").value("政治"))
+                .andExpect(status().isOk());
+    }
+}
