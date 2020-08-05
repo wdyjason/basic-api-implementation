c61d5847e5a00c6ba65031407cad8cc1c078cdf9
wdyjason <ddd123321456555@qq.com>
2020-08-05 22:26:33 +0800

    add api to get users

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -5,6 +5,7 @@ import com.thoughtworks.rslist.validation.ValidationGroup;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,11 @@ public class UserController {
     public ResponseEntity registerUser(@RequestBody @Validated(ValidationGroup.class) User user) {
         userList.add(user);
         return new ResponseEntity("index: " + (userList.size() - 0), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userList;
     }
 
 }
--- src/main/java/com/thoughtworks/rslist/domain/User.java
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.rslist.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.thoughtworks.rslist.validation.FirstValidation;
 import com.thoughtworks.rslist.validation.SecondValidation;
 import lombok.AllArgsConstructor;
@@ -15,20 +16,25 @@ public class User {
 
     @Size(max = 8, groups = SecondValidation.class)
     @NotNull(groups = FirstValidation.class)
+    @JsonProperty("user_name")
     private String userName;
 
     @Min(value = 18, groups = SecondValidation.class)
     @Max(value = 100, groups = SecondValidation.class)
     @NotNull(groups = FirstValidation.class)
+    @JsonProperty("user_age")
     private Integer age;
 
     @NotNull(groups = FirstValidation.class)
+    @JsonProperty("user_gender")
     private String gender;
 
     @Email(groups = FirstValidation.class)
+    @JsonProperty("user_email")
     private String email;
 
     @NotNull(groups = FirstValidation.class)
     @Pattern(regexp = "1\\d{10}", groups = SecondValidation.class)
+    @JsonProperty("user_phone")
     private String phone;
 }
